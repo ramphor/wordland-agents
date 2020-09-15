36f88302e064534687a154727e188d70e0b8a09a
Puleeno Nguyen <puleeno@gmail.com>
2020-09-15 22:03:27 +0700

    Create plugin info

---
+++ wordland-agents.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: WordLand Agents
+ * Author: Ramphor Premium
+ * Author URI: https://puleeno.com
+ * Description:  Create and management agents for WordLand
+ * Version: 1.0.0
+ */
