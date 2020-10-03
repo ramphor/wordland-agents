991f8667ebf6a001da047379f66c9ff5cc747ffc
Puleeno Nguyen <puleeno@gmail.com>
2020-10-04 01:46:36 +0700

    Added agents widget

--- src/Agents/Integration/Elementor/Widgets/AgentsWidget.php
+++ src/Agents/Integration/Elementor/Widgets/AgentsWidget.php
@@ -5,5 +5,36 @@ use Elementor\Widget_Base;
 
 class AgentsWidget extends Widget_Base
 {
+    public function get_name()
+    {
+        return 'wordland_agents_agents';
+    }
 
+    public function get_title()
+    {
+        return __('WordLand Agents', 'wordland_agents');
+    }
+
+    public function get_icon()
+    {
+        return 'eicon-person';
+    }
+
+    public function get_categories()
+    {
+        return array('general', 'landpress');
+    }
+
+    protected function _register_controls()
+    {
+    }
+
+    protected function render()
+    {
+        echo 'agents day';
+    }
+
+    protected function _content_template()
+    {
+    }
 }
