fcf50c5e23fcebaca9e705ab9dfeb63aaaa4fd92
Puleeno Nguyen <puleeno@gmail.com>
2021-03-04 05:30:29 +0700

    Add submenu for WordLand agents

--- includes/Agents/Admin/MenuDashboard.php
+++ includes/Agents/Admin/MenuDashboard.php
@@ -1,24 +1,31 @@
 <?php
 namespace WordLand\Agents\Admin;
 
-class MenuDashboard
-{
+class MenuDashboard {
     private static $instance;
 
-    public static function create_admin_page()
-    {
+    public static function create_admin_page() {
         if (is_null(static::$instance)) {
             static::$instance = new static();
         }
         return static::$instance;
     }
 
-    private function __construct()
-    {
+    private function __construct() {
         add_action('admin_menu', array($this, 'register_admin_menu'));
     }
 
-    public function register_admin_menu()
-    {
+    public function register_admin_menu() {
+        add_submenu_page(
+            'wordland',
+            __('WordLand Agents', 'wordland_agents'),
+            __('Agents', 'wordland_agents'),
+            'manage_options',
+            'wordland_agents',
+            array($this, 'render_ui'),
+        );
+    }
+
+    public function render_ui() {
     }
 }
