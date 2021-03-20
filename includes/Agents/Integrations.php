a03e533b19d480c2e0a47b0f2f71370b0fdc3a44
Puleeno Nguyen <puleeno@gmail.com>
2021-03-20 15:25:04 +0700

    Load agent via PropertyBuilder with scope is single

--- includes/Agents/Integrations.php
+++ includes/Agents/Integrations.php
@@ -2,18 +2,22 @@
 namespace WordLand\Agents;
 
 use WordLand\Agents\Integration\SpreadsheetImporter\MultiAgents;
+use WordLand\Agents\Integration\PropertyBuilder;
 
 class Integrations
 {
     protected $multiAgents;
+    protected $propertyBuilder;
 
     public function __construct()
     {
         $this->multiAgents = new MultiAgents();
+        $this->propertyBuilder = new PropertyBuilder();
     }
 
     public function integrate()
     {
         add_action('init', array($this->multiAgents, 'init'));
+        add_action('init', array($this->propertyBuilder, 'init'));
     }
 }
